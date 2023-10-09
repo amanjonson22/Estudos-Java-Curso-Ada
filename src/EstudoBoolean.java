8bff30370960aedf62a69e9bd90bf6f6bc1916f3
Amanda Caroline <amandacsr10@gmail.com>
2023-10-09 20:40:55 -0300

    create estudo boolean

---
+++ src/EstudoBoolean.java
@@ -0,0 +1,14 @@
+public class EstudoBoolean {
+    public static void main(String[] args){
+        boolean fimDeSemana = true;
+        boolean fazendoSol = true;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+
+        System.out.println(vamosAPraia);
+        
+
+        // <--- faz comentário
+
+    }
+}
