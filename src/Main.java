e07bce9e81abd4c93adb5878b1ba277127e2acc1
Amanda Caroline <amandacsr10@gmail.com>
2023-10-09 18:42:04 -0300

    create project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome;
+
+        nome = "Amanda";
+        System.out.println("Olá, " + nome);
+
+        nome = "Fernanda";
+        System.out.println("Olá, " + nome);
+    }
+}
