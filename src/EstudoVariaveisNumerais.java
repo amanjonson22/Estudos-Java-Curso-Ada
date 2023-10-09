6cabb39ee67fe7433fbb57a33dadcbb11d64fd74
Amanda Caroline <amandacsr10@gmail.com>
2023-10-09 19:43:40 -0300

    changes

--- src/EstudoVariaveisNumerais.java
+++ src/EstudoVariaveisNumerais.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class EstudoVariaveisNumerais {
+public class EstudoVariaveisNumerais {
+    public static void main(String[] args){
+
+        int a;
+        int b = 2;
+
+        a = 3;
+
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println(a + "+" + b + "=" + soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
 }
