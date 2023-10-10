ab7bf874a8161669d0a21f68e9689aa604ea5fd4
Amanda Caroline <amandacsr10@gmail.com>
2023-10-10 10:56:17 -0300

    edit estudoLacosNumericos

--- src/EstudoLacosNumericos.java
+++ src/EstudoLacosNumericos.java
@@ -1,5 +1,11 @@
 public class EstudoLacosNumericos {
     public static void main(String[] args) {
+        // 1 2 3 4 5 6 7 8 9 10
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++){
+                System.out.println(j + "x" + i + " = " + (j * i));
+            }
+        }
         
     }
 }
